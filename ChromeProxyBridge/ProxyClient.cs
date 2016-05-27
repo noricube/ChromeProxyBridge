75aaa8c83451342cfbc73b47177ddfa35d1a9994
nori Seokju Yun <sjyun@smilegate.com>
2016-05-27 20:54:41 +0900

    연결에 따라 다르게 처리하는부분 하나로 처리하도록 리팩토링 .gitignore 파일 추가

--- ChromeProxyBridge/ProxyClient.cs
+++ ChromeProxyBridge/ProxyClient.cs
@@ -13,6 +13,13 @@ namespace ChromeProxyBridge
         const int CR = 13;
         const int LF = 10;
 
+        public enum ClientType
+        {
+            SOCKS4,
+            SOCKS4a,
+            SOCKS5
+        };
+
         public Socket Client { get; }
         public Socket GoogleProxy { get; set; }
 
@@ -36,147 +43,145 @@ namespace ChromeProxyBridge
         {
             try
             {
-                bool isConnect = false;
+                ClientType clientType = ClientType.SOCKS4;
 
-                byte[] buffer = new byte[16384];
-                Dictionary<string, string> headerDict = new Dictionary<string, string>();
-                byte[] modifiedHeader = null;
-                string prevHeader = null;
+                byte[] buffer = new byte[8196];
 
-
-                int pos = 0;
-
-                int readPointer = 0;
-
+                string host = "";
+                int port = 0;
+                
                 // > Client Hello
-                int bytes = Client.Receive(buffer, pos, buffer.Length - pos, SocketFlags.None);
+                int bytes = Client.Receive(buffer, 0, buffer.Length, SocketFlags.None);
 
                 if (buffer[0] == 0x04)
                 {
-                    Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-                    string timestamp = unixTimestamp.ToString();
+                    clientType = ClientType.SOCKS4;
 
-                    int port = buffer[2] * 256 + buffer[3];
+                    port = buffer[2] * 256 + buffer[3];
+                    host = buffer[4].ToString() + "." + buffer[5].ToString() + "." + buffer[6].ToString() + "." + buffer[7].ToString();
 
-                    Int32 IP1 = Convert.ToInt32(buffer[4]);
-                    Int32 IP2 = Convert.ToInt32(buffer[5]);
-                    Int32 IP3 = Convert.ToInt32(buffer[6]);
-                    Int32 IP4 = Convert.ToInt32(buffer[7]);
-                    long ipInt = IP1 + (IP2 * 256) + (IP3 * 256 * 256) + (IP4 * 256 * 256 * 256);
-
-                    string ip = IP1.ToString() + "." + IP2.ToString() + "." + IP3.ToString() + "." + IP4.ToString();
-
-                    //Console.WriteLine("CONNECT " + ip + ":" + port + " HTTP/1.1\r\n" + "Chrome-Proxy: ps=" + timestamp + "-0-0-0, sid=" + Util.CalculateMD5Hash(timestamp + "ac4500dd3b7579186c1b0620614fdb1f7d61f944" + timestamp) + ", b=2214, p=115, c=win\r\n\r\n");
-                    modifiedHeader = Encoding.UTF8.GetBytes("CONNECT " + ip + ":" + port + " HTTP/1.1\r\n" + "Chrome-Proxy: ps=" + timestamp + "-0-0-0, sid=" + Util.CalculateMD5Hash(timestamp + "ac4500dd3b7579186c1b0620614fdb1f7d61f944" + timestamp) + ", b=2214, p=115, c=win\r\n\r\n");
-
-                    GoogleProxy = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                    GoogleProxy.Connect("ssl.googlezip.net", 443);
-
-                    GoogleProxy.Send(modifiedHeader);
-
-                    int recv = GoogleProxy.Receive(buffer, buffer.Length, SocketFlags.None);
-
-                    var responseHeader = Encoding.UTF8.GetString(buffer, 0, recv);
-
-                    Console.WriteLine(responseHeader);
-                    if (responseHeader.IndexOf("200") == -1)
+                    if (buffer[4] == 0 && buffer[5] == 0 && buffer[6] == 0 && buffer[7] != 0)
                     {
-
-                        Client.Send(new byte[] { 0x00, 0x5B, 0, 0, 0, 0, 0, 0 });
-                        Client.Close(10);
-                        return;
-                    }
-
-                    Client.Send(new byte[] { 0x00, 0x5A, 0, 0, 0, 0, 0, 0 });
-                    GoogleProxy.NoDelay = true;
-                    Client.NoDelay = true;
-
-                    var ev = new SocketAsyncEventArgs();
-                    ev.SetBuffer(new byte[8196], 0, 8196);
-                    ev.Completed += OnGoogleReceived;
-                    if (GoogleProxy.ReceiveAsync(ev) == false)
-                    {
-                        OnGoogleReceived(GoogleProxy, ev);
-                    }
-
-                    ev = new SocketAsyncEventArgs();
-                    ev.SetBuffer(new byte[8196], 0, 8196);
-                    ev.Completed += OnClientReceived;
-
-                    if (Client.ReceiveAsync(ev) == false)
-                    {
-                        OnClientReceived(GoogleProxy, ev);
+                        // string host name
+                        bool firstZero = true;
+                        int userIdEndPos = 0;
+                        int domainEndPos = 0;
+                        for (int i = 8; i < buffer.Length; i++)
+                        {
+                            if (buffer[i] == 0)
+                            {
+                                if (firstZero == true)
+                                {
+                                    userIdEndPos = i;
+                                    firstZero = false;
+                                }
+                                else
+                                {
+                                    domainEndPos = i;
+                                    break;
+                                }
+                            }
+                        }
+                        host = Encoding.UTF8.GetString(buffer, userIdEndPos + 1, domainEndPos - userIdEndPos);
                     }
                 }
                 else
                 {
+                    clientType = ClientType.SOCKS5;
+
                     // < Server Hello
                     Client.Send(new byte[] { 0x05, 0x00 });
                     // > client connect
                     bytes = Client.Receive(buffer, 0, buffer.Length, SocketFlags.None);
-
-
-                    if (buffer[3] != 1)
+                    if (bytes == 0)
                     {
-                        throw new Exception("what the hell");
-                    }
-                    Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-                    string timestamp = unixTimestamp.ToString();
-
-                    int port = buffer[8] * 256 + buffer[9];
-
-                    Int32 IP1 = Convert.ToInt32(buffer[4]);
-                    Int32 IP2 = Convert.ToInt32(buffer[5]);
-                    Int32 IP3 = Convert.ToInt32(buffer[6]);
-                    Int32 IP4 = Convert.ToInt32(buffer[7]);
-                    long ipInt = IP1 + (IP2 * 256) + (IP3 * 256 * 256) + (IP4 * 256 * 256 * 256);
-
-                    string ip = IP1.ToString() + "." + IP2.ToString() + "." + IP3.ToString() + "." + IP4.ToString();
-
-                    //Console.WriteLine("CONNECT " + ip + ":" + port + " HTTP/1.1\r\n" + "Chrome-Proxy: ps=" + timestamp + "-0-0-0, sid=" + Util.CalculateMD5Hash(timestamp + "ac4500dd3b7579186c1b0620614fdb1f7d61f944" + timestamp) + ", b=2214, p=115, c=win\r\n\r\n");
-                    modifiedHeader = Encoding.UTF8.GetBytes("CONNECT " + ip + ":" + port + " HTTP/1.1\r\n" + "Chrome-Proxy: ps=" + timestamp + "-0-0-0, sid=" + Util.CalculateMD5Hash(timestamp + "ac4500dd3b7579186c1b0620614fdb1f7d61f944" + timestamp) + ", b=2214, p=115, c=win\r\n\r\n");
-
-                    GoogleProxy = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                    GoogleProxy.Connect("ssl.googlezip.net", 443);
-
-                    GoogleProxy.Send(modifiedHeader);
-
-                    int recv = GoogleProxy.Receive(buffer, buffer.Length, SocketFlags.None);
-
-                    var responseHeader = Encoding.UTF8.GetString(buffer, 0, recv);
-
-                    Console.WriteLine(responseHeader);
-                    if (responseHeader.IndexOf("200") == -1)
-                    {
-
-                        Client.Send(new byte[] { 0x05, 0x03, 0, 0, 0, 0, 0, 0 });
-                        Client.Close(10);
+                        Cleanup();
                         return;
                     }
+                    
+                    port = buffer[8] * 256 + buffer[9];
 
-                    Client.Send(new byte[] { 0x05, 0x00, 0, 0, 0, 0, 0, 0 });
-                    GoogleProxy.NoDelay = true;
-                    Client.NoDelay = true;
-
-                    var ev = new SocketAsyncEventArgs();
-                    ev.SetBuffer(new byte[8196], 0, 8196);
-                    ev.Completed += OnGoogleReceived;
-                    if (GoogleProxy.ReceiveAsync(ev) == false)
+                    if (buffer[3] == 1)
                     {
-                        OnGoogleReceived(GoogleProxy, ev);
+                        host = buffer[4].ToString() + "." + buffer[5].ToString() + "." + buffer[6].ToString() + "." +
+                               buffer[7].ToString();
                     }
-
-                    ev = new SocketAsyncEventArgs();
-                    ev.SetBuffer(new byte[8196], 0, 8196);
-                    ev.Completed += OnClientReceived;
-
-                    if (Client.ReceiveAsync(ev) == false)
+                    else if (buffer[3] == 1)
                     {
-                        OnClientReceived(GoogleProxy, ev);
+                        int hostLength = (int) buffer[4];
+                        host = Encoding.UTF8.GetString(buffer, 5, hostLength);
+                    }
+                    else
+                    {
+                        throw new Exception("wtf");
                     }
                 }
+
+                Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                string timestamp = unixTimestamp.ToString();
+
+                string requestHeader = "CONNECT " + host + ":" + port + " HTTP/1.1\r\n" + "Chrome-Proxy: ps=" + timestamp +
+                                "-0-0-0, sid=" +
+                                Util.CalculateMD5Hash(timestamp + "ac4500dd3b7579186c1b0620614fdb1f7d61f944" + timestamp) +
+                                ", b=2214, p=115, c=win\r\n\r\n";
+
+                Console.WriteLine(" " + host + ":" + port);
+
+                GoogleProxy = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                GoogleProxy.Connect("ssl.googlezip.net", 443);
+
+                GoogleProxy.Send(Encoding.UTF8.GetBytes(requestHeader));
+
+                int recv = GoogleProxy.Receive(buffer, buffer.Length, SocketFlags.None);
+
+                var responseHeader = Encoding.UTF8.GetString(buffer, 0, recv);
+
+                Console.WriteLine(responseHeader);
+                if (responseHeader.IndexOf("200") == -1)
+                {
+
+                    if (clientType == ClientType.SOCKS4)
+                    {
+                        Client.Send(new byte[] { 0x00, 0x5B, 0, 0, 0, 0, 0, 0 });
+                    }
+                    else
+                    {
+                        Client.Send(new byte[] { 0x05, 0x03, 0, 0, 0, 0, 0, 0 });
+                    }
+                    Client.Close(10);
+                    return;
+                }
+
+                if (clientType == ClientType.SOCKS4)
+                {
+                    Client.Send(new byte[] { 0x00, 0x5A, 0, 0, 0, 0, 0, 0 });
+                }
+                else
+                {
+                    Client.Send(new byte[] { 0x05, 0x00, 0, 0, 0, 0, 0, 0 });
+                }
+                GoogleProxy.NoDelay = true;
+                Client.NoDelay = true;
+
+                var ev = new SocketAsyncEventArgs();
+                ev.SetBuffer(new byte[8196], 0, 8196);
+                ev.Completed += OnGoogleReceived;
+                if (GoogleProxy.ReceiveAsync(ev) == false)
+                {
+                    OnGoogleReceived(GoogleProxy, ev);
+                }
+
+                ev = new SocketAsyncEventArgs();
+                ev.SetBuffer(new byte[8196], 0, 8196);
+                ev.Completed += OnClientReceived;
+
+                if (Client.ReceiveAsync(ev) == false)
+                {
+                    OnClientReceived(GoogleProxy, ev);
+                }
             }
-            catch(Exception e)
+
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
@@ -230,7 +235,7 @@ namespace ChromeProxyBridge
 
         private void Cleanup()
         {
-            Console.WriteLine("disconnect ");
+            //Console.WriteLine("disconnect ");
             Client.Close(10);
 
             if (GoogleProxy != null)
